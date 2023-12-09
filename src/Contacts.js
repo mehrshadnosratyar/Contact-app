5a6c034db3534b5465c455abd1948da8361f5eed
Mehrshad Nosratyar <mehrshadnosratyar@gmail.com>
2023-12-09 13:54:26 +0330

    i made a simple contact app with react

--- src/Contacts.js
+++ src/Contacts.js
@@ -1,10 +1,35 @@
-
-function Contacts (){
-    return(
-        <section>
-           no contacts yet!
-        </section>
-    )
+import styles from "./form.module.css"
+    function Contacts ({contacts}){
+        
+        if(contacts.length==0){
+            return(
+            <div>
+               no contacts yet!
+            </div>)
+        } else {
+            return(
+                <ul className={styles.mainlist}>
+                    {contacts.map(contact => (<li className={styles.listitem} key={contact.id}>
+                        {contact.name + " " + contact.lname}
+                        <div>
+                            <p>
+                            <span>📧</span>
+                            {contact.email}
+                            </p>    
+                        </div>
+                        <div>
+                            <p>
+                            <span>📞</span>
+                            {contact.number}
+                            </p>
+                        </div>
+                        <div>
+                            <a href="#">🗑</a>
+                        </div>
+                        </li>))}
+                </ul>
+            )
+        }
 }
 
 
--- src/Form.js
+++ src/Form.js
@@ -1,22 +1,45 @@
+import { useState } from "react";
 import Contacts from "./Contacts";
+import styles from "./form.module.css"
+import {v4} from "uuid";
 function Form (){
+    
+    const [contactsInfo , setContacts] = useState({id:"", name:"",lname:"",email:"",number:""})
+    const [contactProps , setProps] = useState([])
+    const [alert,setAlert] = useState("")
+    function changeHandeler(event) {
+        setContacts( (contactsInfo) => ({...contactsInfo,[event.target.name]:event.target.value}))
+    };
+
+    function submitHandler(){
+        if(!contactsInfo.name||!contactsInfo.lname||!contactsInfo.email||!contactsInfo.number){
+            setAlert("invalid data!")
+            return;
+        }
+        setAlert("")
+        const newContact = {...contactsInfo,id:v4()}
+        setProps(contacts => ([...contacts,newContact]))
+        setContacts({name:"",lname:"",email:"",number:""})
+    }
+
     return(
-        <>
+    <main className={styles.container}>
         <div>
-            <h1>CONTACT APP</h1>
-            <div>
-                <input type="text" placeholder="Name"/>
-                <input type="text" placeholder="Last Name"/>
-                <input  type="email" placeholder="Email"/>
-                <input type="text" placeholder="Number"/>
-                <div>Add contact</div>
+            <h1 className={styles.title}>CONTACT APP</h1>
+            <div className={styles.form}>
+                <input name="name" type="text" value={contactsInfo.name} placeholder="Name" onChange={changeHandeler} />
+                <input name="lname" type="text" value={contactsInfo.lname} placeholder="Last Name"onChange={changeHandeler}/>
+                <input name="email" type="email" value={contactsInfo.email} placeholder="Email" onChange={changeHandeler}/>
+                <input name="number" type="number" value={contactsInfo.number} placeholder="Number" onChange={changeHandeler}/>
+                <div  onClick={submitHandler} className={styles.submit}>Add contact</div>
             </div>
         </div>
-
-        <div>
-            <Contacts/>
+        <div>{alert ? <p>{alert}</p> : null}</div>
+        <div className={styles.contactlist}>
+            <h3 className={styles.listheader} >CONTACTS LIST</h3>
+            <Contacts contacts={contactProps} />
         </div>
-        </>
+    </main>
         )
 }
 
